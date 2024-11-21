bd1ec09cb24cd583699c1e3326bfa81999713771
dawood zia <dawood@gtechsources.com>
2024-11-21 14:34:14 +0500

    add new files

---
+++ src/conversion.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const conversion = () => {
+  return (
+    <View>
+      <Text>conversion</Text>
+      <Text>conversion</Text>
+      <Text>conversion</Text>
+      <Text>conversion</Text>
+    </View>
+  );
+};
+
+export default conversion;
